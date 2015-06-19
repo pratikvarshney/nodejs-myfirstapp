d81c126f5da0d9877571fd60ffd4a0baa29d6174
pratikvarshney <pratikvarshney@users.noreply.github.com>
2015-06-19 10:22:46 +0530

    app.js => mysql server response test

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var express    = require("express");
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -5,12 +6,28 @@ var connection = mysql.createConnection({
   password : 'pass',
   database : 'db1'
 });
+var app = express();
 
-connection.connect();
-
-connection.query('SELECT * from mcqbank', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0].question);
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected ... \n\n");  
+} else {
+    console.log("Error connecting database ... \n\n");  
+}
 });
 
+app.get("/",function(req,res){
+connection.query('SELECT * from mcqbank', function(err, rows, fields) {
 connection.end();
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+
+	var result = rows[0].question.toString();
+	res.send(rows);
+
+  });
+});
+
+app.listen(3000);
