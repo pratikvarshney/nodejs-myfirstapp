61406d81707704d8a55e4c26f788b91dc6d8ede5
pratikvarshney <pratikvarshney@users.noreply.github.com>
2015-06-19 10:09:09 +0530

    app.js => mysql connection test

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'pass',
+  database : 'db1'
 });
 
-var server = app.listen(3000, function () {
+connection.connect();
 
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+connection.query('SELECT * from mcqbank', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].question);
 });
+
+connection.end();
